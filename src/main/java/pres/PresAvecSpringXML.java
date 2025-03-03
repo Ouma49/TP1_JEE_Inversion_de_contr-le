e9d98a079847ecb1173c3a4815ace95182302bbe
Oumayma <oumaymadaoudi1949@gmail.com>
2025-03-03 00:26:02 +0000

    Couche Présentation version Spring_&_XML

---
+++ src/main/java/pres/PresAvecSpringXML.java
@@ -0,0 +1,17 @@
+package pres;
+
+import metier.IMetier;
+import metier.MetierImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class PresAvecSpringXML {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        IMetier metier = (IMetier) context.getBean("metier");
+        System.out.println("RES = " + metier.calcul());
+    }
+}
