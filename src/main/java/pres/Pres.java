f1428127062c81eec44876bac62e7d098d9dbd0f
Oumayma <oumaymadaoudi1949@gmail.com>
2025-03-03 00:25:18 +0000

    Couche Présentation version Spring_&_XML

--- src/main/java/pres/Pres.java
+++ src/main/java/pres/Pres.java
@@ -1,4 +1,15 @@
 package pres;
 
+import dao.DaoImpl;
+import metier.MetierImpl;
+
 public class Pres {
+
+    public static void main(String[] args) {
+      //instanciation statique
+        DaoImpl d = new DaoImpl();
+        MetierImpl metier = new MetierImpl(d);// injection via constructeur
+       // injection via setter //metier.setDao(d);
+        System.out.println("RES = " + metier.calcul());
+    }
 }
